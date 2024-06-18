aa5e279d58d81ee1eed6a669fb0ad12243e74399
Alexandre Direito <xandedsj@gmail.com>
2024-06-18 12:08:20 -0300

    Testando o Consumer

---
+++ Pratica-stream-api/src/ConsumerExemplo.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+//Utilizando o consumer<T>, ele aceita um argumento do tipo T e não retorna nenhum resultado.
+//É utilizado para realizar ações, ou efeitos colaterais nos elementos do Stream sem modificar ou retornar um valor.
+public class ConsumerExemplo {
+    public static void main(String[] args) throws Exception {
+
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
+
+        //Utilizando o Consumer com expressão lambda:
+        Consumer<Integer> imprimirNumeroPar = numero -> {
+            if(numero % 2 == 0) {
+                System.out.println(numero);
+            }
+        };
+
+        // e utilizando o Consumer para imprimir números pares no Stream:
+        numeros.stream().forEach(imprimirNumeroPar);
+    }
+}
