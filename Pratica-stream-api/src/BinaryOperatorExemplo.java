3c599f189d398db705fb51ce72fec3e7592f1690
Alexandre Direito <xandedsj@gmail.com>
2024-06-24 13:44:55 -0300

    otimizando

--- Pratica-stream-api/src/BinaryOperatorExemplo.java
+++ Pratica-stream-api/src/BinaryOperatorExemplo.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.BinaryOperator;
+// import java.util.function.BinaryOperator;
 
 public class BinaryOperatorExemplo {
 
@@ -10,10 +10,12 @@ public class BinaryOperatorExemplo {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
         //Usa o BinaryOperator com expressão lambda para somar dois números inteiros.
-        BinaryOperator<Integer> somar = (num1, num2) -> num1 + num2;
+        // BinaryOperator<Integer> somar = (num1, num2) -> num1 + num2;
 
         //Usa o BinaryOperator para somar todos os números no Stream.
-        int resultado = numeros.stream().reduce(0, somar);
+        int resultado = numeros.stream()
+            .reduce(0, (n1, n2) -> n1 + n2);
+        
 
         //Imprime o resultado final.
         System.out.println("A soma de todos os números é: " + resultado);
